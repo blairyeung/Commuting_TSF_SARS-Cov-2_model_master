665a4e33f55468a7a99abdde8d0067e5e1d623f7
blairyeung <loveblairsky@gmail.com>
2022-08-03 17:00:09 -0400

    updated todo

--- Model/Dependency.py
+++ Model/Dependency.py
@@ -1,5 +1,5 @@
 import os
-import numpy
+import numpy as np
 
 import Parameters
 
@@ -9,11 +9,25 @@ def get_dependency_path():
     return path
 
 def read_matrix():
+    # TODO: Read the matrices in the form of 2D np.array, and put them in a 2D list.
+    """
+        matrix: 16 * 16 float-valued np.array
+        matrix_by_class: 2 * 4 np.array-valued list
+    """
+    matrix_by_class = []
     read_path = get_dependency_path() + 'Matrix_IO/Matrix_by_Category/'
     for category in Parameters.matrix_categories:
         for contact in Parameters.matrix_contact:
             matrix_path = read_path + category + '/' + contact + '/' + Parameters.matrix_country_ISO + '.csv'
             print(matrix_path)
+    return
+
+def read_county_data():
+    # TODO: Read the data of each county in 2D np.array
+    """
+        county_data: 520 * 4 float-valued np.array
+    """
+    county_data = np.array([520, 4])
     return
 
 read_matrix()
--- Model/Parameters.py
+++ Model/Parameters.py
@@ -1,7 +1,33 @@
 """
+    Version
+"""
+
+ver = '1.0'
+
+
+
+"""
     Matrix constants
 """
 matrix_size = 16
 matrix_categories = ['urban', 'rural']
 matrix_contact = ['home', 'school', 'work', 'others']
 matrix_country_ISO = 'CA'
+
+"""
+    Geo constants
+"""
+province_name = 'Ontario'
+num_phu = 34
+num_county = 520
+
+"""
+    Epidemiological constants
+"""
+
+# susceptibility
+sup_by_age = []
+# fatality rate (calculated)
+cfr_by_age = []
+# critical rate
+crit_by_age = []
