a8b1404a5e2426e7fef9f9bdffd757715bd87f33
blairyeung <loveblairsky@gmail.com>
2022-08-03 16:35:35 -0400

    Dependency.py

--- Model/Dependency.py
+++ Model/Dependency.py
@@ -1,3 +1,13 @@
-class Dependency:
-    def read_matrix(self):
-        return
+import os
+import numpy
+
+def get_dependency_path():
+    path = os.getcwd()[:-5] + 'Model Dependencies/'
+    return path
+
+def read_matrix():
+    matrix_path = get_dependency_path() + 'Matrix_IO/Matrix_by_Category/'
+    print(matrix_path)
+    return
+
+read_matrix()
