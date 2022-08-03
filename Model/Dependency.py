afe4f5a1e203e3b0667cbb570b05a881ba5ad860
blairyeung <loveblairsky@gmail.com>
2022-08-03 16:41:28 -0400

    Dependency.py

--- Model/Dependency.py
+++ Model/Dependency.py
@@ -1,13 +1,19 @@
 import os
 import numpy
 
+import Parameters
+
+
 def get_dependency_path():
     path = os.getcwd()[:-5] + 'Model Dependencies/'
     return path
 
 def read_matrix():
-    matrix_path = get_dependency_path() + 'Matrix_IO/Matrix_by_Category/'
-    print(matrix_path)
+    read_path = get_dependency_path() + 'Matrix_IO/Matrix_by_Category/'
+    for category in Parameters.matrix_categories:
+        for contact in Parameters.matrix_contact:
+            matrix_path = read_path + category + '/' + contact + '/' + Parameters.matrix_country_ISO + '.csv'
+            print(matrix_path)
     return
 
 read_matrix()
---
+++ Model/Parameters.py
@@ -0,0 +1,7 @@
+"""
+    Matrix constants
+"""
+matrix_size = 16
+matrix_categories = ['urban', 'rural']
+matrix_contact = ['home', 'school', 'work', 'others']
+matrix_country_ISO = 'CA'
