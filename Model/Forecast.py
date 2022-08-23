4611298585ef032a832af04ab1bcaa40c1cbc53d
blairyeung <53135008+blairyeung@users.noreply.github.com>
2022-08-23 19:12:57 -0400

    MLP model added

--- Model/Forecast.py
+++ Model/Forecast.py
@@ -1,3 +1,14 @@
+import matplotlib.pylab as plt
 import numpy as np
 import pandas as pd
+import os
 
+for i in [2020, 2021, 2022]:
+    pass
+path = os.getcwd()[:-5] + 'Model Dependencies/Mobility_data/2020_CA_Region_Mobility_Report.csv'
+print(path)
+data = pd.read_csv(path)
+print(data)
+mobility = data.ix[:, 'retail_and_recreation_percent_change_from_baseline'].tolist()
+# plt.plot(mobility)
+# plt.show()
