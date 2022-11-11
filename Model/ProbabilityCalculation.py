0e87dab734dce3e16708ec024d2d57b3851f45e9
blairyeung <53135008+blairyeung@users.noreply.github.com>
2022-11-10 23:54:31 -0500

    Working on the data, should be able to finish by this week.

--- Model/ProbabilityCalculation.py
+++ Model/ProbabilityCalculation.py
@@ -1,3 +1,4 @@
+import cv2
 import pandas as pd
 import os
 import csv
@@ -7,20 +8,26 @@ from datetime import datetime
 import Parameters
 
 
-death_rate_by_pr = None
+cfr_by_pr = None
 no_cases_by_pr = None
 no_hosp_by_pr = None
 no_icu_by_pr = None
 no_death_by_pr = None
 
+
+def fill_array(lst):
+    for i in range(1, len(lst)):
+        if lst[i] == 0:
+            lst[i] = lst[i-1]
 def to_days(lst):
     new_lst = []
+    date_lst = []
     for i in lst:
         this_day = datetime.strptime(i, '%Y-%m-%d')
         after_outbreak = (this_day - Parameters.OUTBREAK_FIRST_DAY).days
         new_lst.append(after_outbreak)
-        #print(after_outbreak)
-    return new_lst
+        date_lst.append(this_day)
+    return new_lst, date_lst
 
 def get_cases_total():
     read_path = d.get_dependency_path() + 'covid19-download.csv'
@@ -30,16 +37,41 @@ def get_cases_total():
     grouped = df.groupby(df.prname)
     # print(grouped)
 
-    no_cases_by_pr = [None] * len(grouped.groups)
+    no_cases_by_pr = dict()
+    cfr_by_pr = dict()
 
     for i in range(len(grouped.groups)):
-        group = grouped.get_group(grouped.groups[i])
+        # print(grouped.groups.keys())
+        group_name = list(grouped.groups.keys())[i]
+        group = grouped.get_group(group_name)
         time = group['date'].tolist()
-        cases = np.array(group['avgcases_last7'].tolist())
-        deaths = np.array(group['numdeaths_last7'].tolist())
+        cases = np.clip(np.array(group['totalcases'].tolist()), a_max=100000000000, a_min=1)
+        deaths = np.array(group['numdeaths'].tolist())
+        # print(cases)
+        # print(deaths)
+        cfr = np.divide(deaths, cases)
+        # print(cfr)
         # time = group.tolist()
-        dates = to_days(time)
-        no_cases_by_pr[i] = data
+        indices, dates = to_days(time)
+        data = np.zeros(shape=(max(indices)+1))
+        for j in range(len(indices)):
+            data[indices[j]] = cases[j]
+        fill_array(data)
+        length = data.shape[0]
+        data = np.reshape(np.array(cv2.GaussianBlur(data, (25, 25), 5)), newshape=(length, ))
+        print(data.shape)
+        no_cases_by_pr[group_name] = list(np.round(data))
+        cfr_by_pr[group_name] = cfr
+
+   #  print(no_cases_by_pr)
+
+    # cfr_df = pd.DataFrame(data=cfr_by_pr)
+    cases_df = pd.DataFrame(data=no_cases_by_pr)
+
+    # cfr_df.to_csv(d.get_dependency_path() + 'cfr_by_pr.csv')
+    cases_df.to_csv(d.get_dependency_path() + 'cases_by_pr.csv')
+
+
     pass
 
 def get_hosp_total():
